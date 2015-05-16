771695f60320667f3a49b57cf51461c559d5d156
Amy Dusek <amlydu@umich.edu>
2015-05-16 14:22:31 -0400

    Added Album scaffold

---
+++ app/views/albums/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @album, :id, :name, :release_date, :photo, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150516181628) do
+ActiveRecord::Schema.define(version: 20150516182002) do
+
+  create_table "albums", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "release_date"
+    t.string   "photo"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
