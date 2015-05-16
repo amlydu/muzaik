98fed46ef197846306bf6ce091a3d2ee0b783d68
Steven Worley <bitcopia@gmail.com>
2015-05-16 18:00:43 -0400

    removed commented nonsense

--- app/services/wiki_api.rb
+++ app/services/wiki_api.rb
@@ -9,22 +9,4 @@ class WikiApi
 			self.class.get("/#{artist.squish.tr(" ","_")}_(#{type})")
 		end
 	end
-
-	# def parsed_artist_info
-		
-	# end
- #      dom = Nokogiri::HTML(response.body)
- #      if dom.include? "Discography"
- #        biography = dom.css("#mw-content-text p").first
- #        @artist.biography = biography.content
- #      elsif !(dom.include? "Discography") &&  
- #        url += "_(musician)"
- #        response = HTTParty.get(url)
- #        dom = Nokogiri::HTML(response.body)
- #        summary = dom.css("#mw-content-text p").first
- #        @artist.summary = summary.content
- #      elsif !dom.include? "Discography"
- #      	url += "_(group)"
-
- #        types = [nil, _(musician), _(group), _(band)]
 end
