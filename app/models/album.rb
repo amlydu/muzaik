ac3bef3f3813f34dbfa10db4e51947d9667b7a38
Amy Dusek <amlydu@umich.edu>
2015-05-16 15:14:51 -0400

    Added relationships to models

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,6 @@
 class Album < ActiveRecord::Base
+  belongs_to :artist
+  has_many :songs
+  has_many :ratings
+  has_many :users, through: :ratings
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,5 @@
 class Artist < ActiveRecord::Base
+  has_many :albums
+  #songs we're seeing if this is inherited through albums
+
 end
