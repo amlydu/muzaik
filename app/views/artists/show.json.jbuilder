90c32f229439569cc4c2307187956d7534db1e04
Amy Dusek <amlydu@umich.edu>
2015-05-16 14:17:59 -0400

    Artist scaffold generated

---
+++ app/views/artists/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @artist, :id, :name, :biography, :photo, :genre, :created_at, :updated_at
---
+++ db/migrate/20150516181628_create_artists.rb
@@ -0,0 +1,12 @@
+class CreateArtists < ActiveRecord::Migration
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.text :biography
+      t.string :photo
+      t.string :genre
+
+      t.timestamps null: false
+    end
+  end
+end
