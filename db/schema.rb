9836e2fbba6f9c960a1a9fdb2a481f6b3948ae06
Amy Dusek <amlydu@umich.edu>
2015-05-16 16:08:36 -0400

    added artist id migration to albums table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150516184146) do
+ActiveRecord::Schema.define(version: 20150516200121) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150516184146) do
     t.string   "photo"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "artist_id"
   end
 
   create_table "artists", force: :cascade do |t|
