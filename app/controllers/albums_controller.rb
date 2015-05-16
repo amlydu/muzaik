6d73c52eae022dbf5ad0e0badb0c50667d3386ff
Amy Dusek <amlydu@umich.edu>
2015-05-16 15:59:40 -0400

    Playing around with trying to associate an album with an artist; will create migration for artist_id in album

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -69,6 +69,6 @@ class AlbumsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def album_params
-      params.require(:album).permit(:name, :release_date, :photo)
+      params.require(:album).permit(:name, :release_date, :photo, :artist_id)
     end
 end
