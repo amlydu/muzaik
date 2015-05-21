3f4fe73680c1a4c49d0a7fd3dfd1f20222e9b0c2
Amy Dusek <amlydu@umich.edu>
2015-05-21 13:46:06 -0400

    Add active to separate dashboard page; add corresponding routes and action

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :artists
 
   root 'artists#index'
+  get 'dashboard' => 'artists#dashboard'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
