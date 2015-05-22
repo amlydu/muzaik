6f525103929687e2d8eeef4896c4549f3330f08a
Amy Dusek <amlydu@umich.edu>
2015-05-22 11:59:11 -0400

    Add Songs by Album path to link in Albums; sorted alphabetically

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   root 'artists#index'
   get 'dashboard' => 'artists#dashboard'
+  get 'album_songs' => 'albums#album_songs'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
