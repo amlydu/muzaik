8e7f337542c7b80da6a54c87da3d56a39aeb8ef6
Steven Worley <bitcopia@gmail.com>
2015-05-25 15:30:13 -0400

    replace + with %20

--- app/services/echo_nest_api.rb
+++ app/services/echo_nest_api.rb
@@ -3,6 +3,6 @@ class EchoNestApi
   base_uri 'http://developer.echonest.com/api/v4/artist/search?api_key=WITDBGZPPHKHUCPLK&format=json&name='
 
   def get_artist_info(artist)
-    self.class.get("#{artist.squish.tr(" ","+")}&bucket=biographies&bucket=images&bucket=genre&bucket=hotttnesss&results=1")
+    self.class.get("#{artist.squish.tr(" ","%20")}&bucket=biographies&bucket=images&bucket=genre&bucket=hotttnesss&results=1")
   end
 end
