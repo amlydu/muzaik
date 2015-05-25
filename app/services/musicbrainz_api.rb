b754c80c24e81d7a7c952ce817f8976f54cee99a
Steven Worley <bitcopia@gmail.com>
2015-05-25 16:06:47 -0400

    update musicbrainz api with URI.encode

--- app/services/musicbrainz_api.rb
+++ app/services/musicbrainz_api.rb
@@ -2,6 +2,6 @@ class MusicbrainzApi
   include HTTParty
   
   def get_artist_info(artist)
-    self.class.get("http://musicbrainz.org/ws/2/release-group/?query=artist:%22#{artist.squish.tr(" ","%20")}%22%20AND%20primarytype:%22album%22")
+    self.class.get("http://musicbrainz.org/ws/2/release-group/?query=artist:%22#{URI.encode(artist.squish)}%22%20AND%20primarytype:%22album%22")
   end
 end
