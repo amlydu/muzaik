8d12570e714b78e2a0a5c4e746d3f638b92d9fdc
Kevin Ridge <mrrydge91@gmail.com>
2015-05-27 10:36:09 -0400

    fix development solr

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'tranquil-earth-1333.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
+  Sunspot.config.solr.url = ENV['https://index.websolr.com/solr/c07bda7a643'] #search stuff
 
   # Code is not reloaded between requests.
   config.cache_classes = true
